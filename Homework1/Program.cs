c71583e660e3afabb8fdd461ffa9fdad8110d7c4
Dimass14 <dimassik2205@gmail.com>
2022-12-01 16:51:58 +1000

    Homework1

--- Homework1/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-Console.Write("Input number a: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input number b: ");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(Math.Pow(a,b));
